fd446e6c711ab9a96cab4affdf3119ab43541ede
TeaBoss <104977213+TeaBoss-Developer@users.noreply.github.com>
2023-04-11 20:37:35 +0800

    Add files via upload

---
+++ cookie_autologin.py
@@ -0,0 +1,20 @@
+import time
+from selenium.webdriver.common.by import By
+
+from selenium import webdriver
+browser = webdriver.Chrome(r"C:\Program Files\Google\Chrome\Application\chromedriver.exe")
+browser.maximize_window()
+browser.get("https://qzone.qq.com/")
+time.sleep(1.2)
+browser.switch_to.frame('login_frame')
+browser.find_element(By.ID,'switcher_plogin').click()
+time.sleep(1)
+browser.find_element(By.ID,'u').send_keys("1844002667")
+browser.find_element(By.ID,'p').send_keys("159357456q")
+time.sleep(1)
+browser.find_element(By.ID,'login_button').click()
+time.sleep(10)
+cookies_list = browser.get_cookies()
+for cookie in cookies_list:
+    if 'name' in cookie and 'value' in cookie:
+        print(cookie['name']+"|"+cookie['value'])
